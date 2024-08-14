9136d7163c82dea95506ebf9443256261d91a20e
Pras2005 <146535227+Pras2005@users.noreply.github.com>
2024-08-14 14:11:53 +0530

    user functionality

--- user/models.py
+++ user/models.py
@@ -5,4 +5,4 @@ class notes(models.Model):
     title = models.CharField(max_length=255)
     year = models.IntegerField(max=2)
     subject = models.CharField(max_length=100)
-    url = models.URLField(max_length=2000)
+    url = models.FileField(upload_to ='satic/notes/')
--- user/views.py
+++ user/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect, HttpResponse
+from .models import*
 
 # Create your views here.
+def landing(request):
+    return render(request, 'landing.html')
+
+def noteEntry(request):
+    if request.method == "POST" :
+        title = request.POST.get("title")
+        year = request.POST.get("year")
+        subject = request.POST.get("subject")
+        url = request.FILES.get("url")
+        if title and subject and year and url :
+            n = notes.objects.create (
+            title = title,
+            year = year,
+            subject = subject,
+            url = url )
+            n.save()
+        else:
+            return HttpResponse("Please fill all fields")
+        return render(request, 'noteEntry.html')
+    
+
+def displayNotes(request):
+     notes = notes.objects.all()
+     return render(request , "show.html",{"notes":notes})
+
+        
+
