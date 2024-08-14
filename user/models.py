8fdd8c083e6fa66310aaa0103c95b19a9eb95699
ashish-nagmoti <ashishnagmoti5@gmail.com>
2024-08-14 13:30:12 +0530

    added model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class notes(models.Model):
+    title = models.CharField(max_length=255)
+    year = models.IntegerField(max=2)
+    subject = models.CharField(max_length=100)
+    url = models.URLField(max_length=2000)
