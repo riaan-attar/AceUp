c7924c86b647f6e67c7ac00b74b3ce14e1900017
Pras2005 <146535227+Pras2005@users.noreply.github.com>
2024-08-15 12:55:17 +0530

    pdf retirval system for genai

--- learnGpt/views.py
+++ learnGpt/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render , render , redirect, HttpResponse
 from user.models import notes
+import pdfplumber as pp
 
+def gen(request):
+    if request.method == "POST":
+        sub = request.POST.get("subject")
+        year = request.post.get("year")
+        question = request.POST.get("question")
+        pdf_urls = notes.objects.filter(year =year,subject = sub)
+        extracted_text =""
+        for pdf_path  in pdf_urls:
+            with pp.open(pdf_path) as pdf:
+                for page in pdf.pages:
+                    # Extract text from each page
+                    page_text = page.extract_text()
+                    extracted_text += page_text
+        
 
