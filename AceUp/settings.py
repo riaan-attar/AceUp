f67c09347af3bdf4a1efeaa77897a7c1cf8087e3
Ashish Nagmoti <135442863+ashish-nagmoti@users.noreply.github.com>
2024-08-19 00:08:25 +0530

    Update settings.py

--- AceUp/settings.py
+++ AceUp/settings.py
@@ -50,9 +50,9 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-X_FRAME_OPTIONS = 'SAMEORIGIN'
+# X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ROOT_URLCONF = 'AceUp.urls'
 
