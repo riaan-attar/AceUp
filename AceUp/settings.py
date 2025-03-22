7b6601eedad858e9cf03c72b9ba9dc2a38b81081
ashish-nagmoti <ashishnagmoti5@gmail.com>
2025-03-22 09:00:37 +0530

    Api changes

--- AceUp/settings.py
+++ AceUp/settings.py
@@ -12,10 +12,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+GEMINI_API = os.getenv("GEMINI_API")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
--- learnGpt/views.py
+++ learnGpt/views.py
@@ -10,12 +10,14 @@ dotenv.load_dotenv()
 
 def gen(request):
     if request.method == "POST":
+        
         sub1 = request.POST.get("subject")
         sub = sub1.lower()
         year = request.POST.get("year")
         question = request.POST.get("question")
         pdf_urls = notes.objects.filter(year=year, subject=sub)
         
+ 
         extracted_text = ""
         for pdf_note in pdf_urls:
             with pp.open(pdf_note.url.path) as pdf:
@@ -24,15 +26,16 @@ def gen(request):
                     extracted_text += page_text
         
         text = extracted_text
-        print(extracted_text)
-        # api_key = os.getenv("GOOGLE_API_KEY")
-        api_key = "AIzaSyBW5Cxl9693EfEKg_9SJ67inkoZbWYvPYs"
+        
+        api_key = os.getenv("GEMINI_API")
+        
         try:
             genai.configure(api_key=api_key)
             
             
-            model = genai.GenerativeModel('gemini-1.5-flash')
+            model = genai.GenerativeModel('gemini-2.0-flash')
             response = model.generate_content(f"Assume you are an expert in the topic of {sub} and you have been asked {question} by a student studying in year {year} of engineering. Answer the question comprehensively with the primary context of notes text: {text}. Also, add additional essential information if necessary, return the text STRICTLY in human understanble language, without any special characters such as # /n or ** ins response.")
+            print(response)
             candidates = getattr(response, 'candidates', [])
             if candidates:
                 candidate = candidates[0]
@@ -58,6 +61,7 @@ def gen(request):
                 'generated_text': generated_text,
                 'question': question,
             }
+            print(context)
             return JsonResponse(context)
         
         except Exception as e:
